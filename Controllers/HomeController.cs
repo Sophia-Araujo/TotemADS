9b330c4891085590569f5a85c5f120e0b648a3a5
mkrosz <102179118+mkrosz@users.noreply.github.com>
2025-04-14 20:50:36 -0300

    atualizando controller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -48,6 +48,11 @@ public class HomeController : Controller
         return View();
     }
 
+        public IActionResult telaItensPedidos()
+    {
+        return View();
+    }
+
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
