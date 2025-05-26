f71d07e77d0fe424d2ed00fe485314c1f33216eb
rafariganti <rafaelariganti@gmail.com>
2025-05-26 17:37:28 -0300

    arrumando os arquivos anteriores de pagamento

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -68,9 +68,10 @@ public class HomeController : Controller
         return View();
     }
 
-
-
-
+   public IActionResult PagamentosCartao()
+   {
+    return View();
+   }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
