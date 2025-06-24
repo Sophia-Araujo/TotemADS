ee7690a07f19145c5a4368b468374c65eb10c19a
Ronaldo Araujo Silva Junior <ronaldogamer2006@gmail.com>
2025-06-24 14:10:07 -0300

    hotfixDB

---
+++ Migrations/20250624170817_hotfixModels.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TotemPWA.Migrations
+{
+    /// <inheritdoc />
+    public partial class hotfixModels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "ProdutoId",
+                table: "Cupons",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Codigo",
+                table: "Cupons",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cupons_ProdutoId",
+                table: "Cupons",
+                column: "ProdutoId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cupons_Produto_ProdutoId",
+                table: "Cupons",
+                column: "ProdutoId",
+                principalTable: "Produto",
+                principalColumn: "ProdutoId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cupons_Produto_ProdutoId",
+                table: "Cupons");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Cupons_ProdutoId",
+                table: "Cupons");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProdutoId",
+                table: "Cupons",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Codigo",
+                table: "Cupons",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+        }
+    }
+}
